dbd97480cd15a4ec722647d778f6e439cdf4b533
Elliot Short <elliot.short@dvla.gov.uk>
2026-09-21 14:38:29 +0100

    Fixing styling

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Set basePath and assetPrefix for GitHub Pages deployment
+  basePath: isGithubPages ? '/mmtc' : '',
+  assetPrefix: isGithubPages ? '/mmtc/' : '',
 };
 
 module.exports = nextConfig;
